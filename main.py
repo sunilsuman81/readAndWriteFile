0d63b384d976d2093550f760f77df7d093b0b845
Sunil Suman <99410256+sunilsuman81@users.noreply.github.com>
2024-11-29 22:13:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+with open("") as file:
+    contents = file.read()
+    print(contents)
+
+# with open("my_file.txt", mode="a") as file:
+#     contents = file.write("\n I like to walk in morning")
+#     print(contents)
+#
+# with open("new_file.txt", mode="a") as newFile:
+#     contents = newFile.write("Hi This is new file")
